1091c3afdc8a42e56e859f881af0ff884563005e
conradgabe <gisuekebho5880@gmail.com>
2022-09-19 15:24:35 +0000

    registered the models

--- HackerNewApp/admin.py
+++ HackerNewApp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News, Story, Comment, Ask, Job, Poll, Pollopt
 
 admin.site.register(News)
+admin.site.register(Story)
+admin.site.register(Comment)
+admin.site.register(Ask)
+admin.site.register(Job)
+admin.site.register(Poll)
+admin.site.register(Pollopt)
