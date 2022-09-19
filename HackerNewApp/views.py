18568b21c3fba7b99e3daa62ea5ab616cbb59f02
conradgabe <gisuekebho5880@gmail.com>
2022-09-19 15:27:03 +0000

    configured the logic for the apiview

--- HackerNewApp/views.py
+++ HackerNewApp/views.py
@@ -5,23 +5,78 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import filters, generics
 
-from .models import News
+from .models import News, Story, Comment, Ask, Job, Poll, Pollopt
 from .serializers import ItemSerializer
 from .pagination import CustomPageNumberPagination
 
 def make_request(request):
 
-    while True:
-        url = "https://hacker-news.firebaseio.com/v0/beststories.json"
+    # while True:
+    url = "https://hacker-news.firebaseio.com/v0/beststories.json"
+
+    payload = "{}"
+    responses = requests.request("GET", url, data=payload).json()
+
+    for response in responses:
+        res = response
+        url =  f"https://hacker-news.firebaseio.com/v0/item/{res}.json"
 
         payload = "{}"
         responses = requests.request("GET", url, data=payload).json()
 
-        for response in responses[0:100]:
-            # print(response)
-            # return response
-            db = News.objects.create(item_id=response)
-            db.save()
+        db = News.objects.create(
+                    author=responses['by'], item_id=responses['id'], text=responses['text'], descendants=responses['descendants'], 
+                    time=responses['time'], title=responses['title'], kids=responses['kids'], parent=responses['parent'],
+                    type_of=responses['type'], score=responses['score'], parts=responses['parts'],  poll=responses['poll'],
+                    url=responses['url'], 
+        )
+        db.save()
+        
+        if responses['type'] == story:
+            type_story = Story.objects.create(
+                    author=responses['by'], item_id=responses['id'], descendants=responses['descendants'],
+                    kids=responses['kids'], time=responses['time'], title=responses['title'], type_of=responses['type'],
+                    url=responses['url'], score=responses['score'],
+            )
+            type_story.save()
+        
+        elif responses['type'] == comment:
+            type_comment = Comment.objects.create(
+                    author=responses['by'], item_id=responses['id'], text=responses['text'],
+                    kids=responses['kids'], parent=responses['parent'], time=responses['time'],
+                    type_of=responses['type']
+            )
+            type_comment.save()
+        
+        elif responses['type'] == ask:
+            type_ask = Ask.objects.create(
+                    author=responses['by'], item_id=responses['id'], descendants=responses['descendants'],
+                    kids=responses['kids'], time=responses['time'], title=responses['title'], type_of=responses['type'],
+                    score=responses['score'], text=responses['text'],
+            )
+            type_ask.save()
+
+        elif responses['type'] == job:
+            type_job = Job.objects.create(
+                    author=responses['by'], item_id=responses['id'], text=responses['text'],
+                    time=responses['time'], title=responses['title'], url=responses['url'],
+                    type_of=responses['type'], score=responses['score'],
+            )
+            type_job.save()
+
+        elif responses['type'] == poll:
+            type_poll = Poll.objects.create(
+                    author=responses['by'], item_id=responses['id'], text=responses['text'], descendants=responses['descendants'], 
+                    time=responses['time'], title=responses['title'], kids=responses['kids'],
+                    type_of=responses['type'], score=responses['score'], parts=responses['parts'],
+            )
+            type_poll.save()
+
+        elif responses['type'] == pollopt:
+            type_pollopt = Pollopt.objects.create(
+                    author=responses['by'], item_id=responses['id'], text=responses['text'], poll=responses['poll'],
+                    score=responses['score'], time=responses['time'], type_of=responses['type'],
+            )
         # time.sleep(300)
 
     return render(request, 'index.html', {'res': response})
