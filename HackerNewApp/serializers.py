1b9e506639b9a84f8ed1243f1beb8a349487cf0e
conradgabe <gisuekebho5880@gmail.com>
2022-09-19 15:26:30 +0000

    serializing the fields

--- HackerNewApp/serializers.py
+++ HackerNewApp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import News
+from .models import News, Story, Comment, Ask, Job, Poll, Pollopt
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
